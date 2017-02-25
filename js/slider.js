d4c042bd0ac795d2ea80b56098679579285febb4
SergeyStepanenko <groverfmx@gmail.com>
2017-02-25 18:00:41 +0300

    768 is done

--- js/slider.js
+++ js/slider.js
@@ -1,70 +1,76 @@
 ; "use strict";
 
 (function sliderAnimation () {
-  var block = document.querySelectorAll(".quotation"),
-  singleBlock = document.querySelector(".quotation"),
-  blockWidth = window.getComputedStyle(singleBlock, null).getPropertyValue("width").replace(/[a-z]/g, ""),
-  blockMargin = window.getComputedStyle(singleBlock, null).getPropertyValue("margin").split(" ").pop().replace(/[a-z]/g, "") * 2,
-  blockRealWidth = +blockWidth + +blockMargin,
+  var blockWidth = document.querySelector(".quotationWidth").clientWidth,
   t1 = "translate3d(",
   t2 = "px, 0, 0)",
   btn1 = document.querySelector("#btn1"),
   btn2 = document.querySelector("#btn2"),
   btn3 = document.querySelector("#btn3"),
   btn4 = document.querySelector("#btn4"),
-  btn5 = document.querySelector("#btn5");
-
-
+  btn5 = document.querySelector("#btn5"),
+  sliderPosition = 0;
 
   document.querySelector("#btn1").onclick = function () {
-    for (var i = 0; i < block.length; i++) {
-      document.querySelectorAll(".quotation")[i].style.transform = "translate3d(0px, 0, 0)";
-    }
+
+    document.querySelector(".quotationWrapper").style.transform = "translate3d(0px, 0, 0)";
+
     btn1.style.backgroundColor = "#D50821";
     btn2.style.backgroundColor = "#E9E9E9";
     btn3.style.backgroundColor = "#E9E9E9";
     btn4.style.backgroundColor = "#E9E9E9";
     btn5.style.backgroundColor = "#E9E9E9";
+     sliderPosition = 0;
   }, false;
 
   document.querySelector("#btn2").onclick = function () {
-    for (var i = 0; i < block.length; i++) {
-      shiftBlocks(1);
-    }
-    btn2.style.backgroundColor = "#D50821";
+    shiftBlocks(1);
+     sliderPosition = 1;
   }, false;
 
   document.querySelector("#btn3").onclick = function () {
-    for (var i = 0; i < block.length; i++) {
-      shiftBlocks(2);
-    }
-    btn3.style.backgroundColor = "#D50821";
+    shiftBlocks(2);
+     sliderPosition = 2;
   }, false;
 
   document.querySelector("#btn4").onclick = function () {
     shiftBlocks(3);
-    btn4.style.backgroundColor = "#D50821";
+     sliderPosition = 3;
   }, false;
 
   document.querySelector("#btn5").onclick = function () {
     shiftBlocks(4);
-    btn5.style.backgroundColor = "#D50821";
+     sliderPosition = 4;
   }, false;
 
+  document.querySelector("#arrowLeft").onclick = function () {
+    if (sliderPosition != 0) {
+      shiftBlocks(sliderPosition - 1);
+      sliderPosition = sliderPosition - 1;
+    }
+  }
 
+  document.querySelector("#arrowRight").onclick = function () {
+    if (sliderPosition != document.querySelectorAll(".quotation").length - 1) {
+      shiftBlocks(sliderPosition + 1);
+      sliderPosition = sliderPosition + 1;
+    }
+  }
 
   function shiftBlocks(times) {
-    for (var i = 0; i < block.length; i++) {
-      document.querySelectorAll(".quotation")[i].style.transform = t1 + -blockRealWidth * times + t2;
-    };
+    var a = "btn",
+    c = ".style.backgroundColor = '#D50821'";
 
-    for (var i = 0; i < block.length; i++) {
+      document.querySelector(".quotationWrapper").style.transform = t1 + -blockWidth * times + t2;
+      // document.querySelector(".quotationWrapper").style.msTransform = t1 + -blockWidth * times + t2;
+
       btn1.style.backgroundColor = "#E9E9E9";
       btn2.style.backgroundColor = "#E9E9E9";
       btn3.style.backgroundColor = "#E9E9E9";
       btn4.style.backgroundColor = "#E9E9E9";
       btn5.style.backgroundColor = "#E9E9E9";
-    };
+
+      eval(a + (times + 1) + c);
   };
 
 }());
