e06473743e02bdb0595b91bae479f4f38f9764a2
SergeyStepanenko <groverfmx@gmail.com>
2017-02-28 19:54:53 +0300

    validation added

--- js/popUp.js
+++ js/popUp.js
@@ -2,26 +2,24 @@
 
 (function popUp () {
   var div = document.createElement('div'),
-  body = document.querySelector("body"),
-  closeButton,
-  overlay;
+  body = document.querySelector("body");
 
   div.className = "popUpWrapper";
   div.innerHTML = "\
-      <div class = 'popUpContainer'>\
-      <a class = 'closeButton'>x</a>\
-        <div class = 'popUpContainerInner'>\
-          <div class = 'callMePopUp'>Перезвоните мне</div>\
-          <div class = 'popUpText'>Введите свое имя и номер телефона и мы сразу же перезвоним</div>\
-          <div class = 'form'>\
-            <form action='/action_page.php'>\
-              <input class = 'inputName' type='text' name='name' value = 'Введите ваше имя'>\
-              <input class = 'inputPhoneNumber' type='text' name='lname' value = '+7 (___) ___-__-__'>\
-              <input class = 'submitData' type='submit' value='Жду звонка!'>\
-            </form>\
-          </div>\
-        </div>\
-      </div>";
+  		<div class = 'popUpContainer'>\
+  				<a class = 'closeButton'>x</a>\
+  					<div class = 'popUpContainerInner'>\
+  						<div class = 'callMePopUp'>Перезвоните мне</div>\
+  						<div class = 'popUpText'>Введите свое имя и номер телефона и мы сразу же перезвоним</div>\
+  						<div class = 'form'>\
+  							<form action='' id='myForm' method>\
+                  <input class='inputName' name='name' value='Введите ваше имя'>\
+  								<input class='inputPhoneNumber' name='phone' value='+7 (___) ___-__-__'>\
+  								<input class='submitData' type='submit' value='Отправить'>\
+  							</form>\
+  		        </div>\
+  		      </div>\
+  		    </div>";
 
 
   document.querySelector('.callBeBackBtn').addEventListener('click', appendChild, false);
@@ -30,6 +28,21 @@
 
   function appendChild () {
     body.appendChild(div);
+    function append () {
+      body.appendChild(div);
+      $("#myForm").submit(function(){
+        var error = "";
+
+        error += $(this).yaproField("phone", "p", "телефон введен неправильно");
+
+        return false;
+      });
+
+      fieldPhone("[name=phone]");
+    };
+
+    setTimeout(append, 1);
+
 
     closeButton = document.querySelector('.closeButton');
     closeButton.addEventListener('click', closePopUp, false);
@@ -38,21 +51,31 @@
 
     document.querySelector('.inputName').addEventListener('click', resetStyleName, false);
     document.querySelector('.inputPhoneNumber').addEventListener('click', resetStylePhone, false);
+
   };
 
   function closePopUp () {
     body.removeChild(div);
     closeButton.removeEventListener('click', closePopUp, false);
     overlay.removeEventListener('click', closePopUp, false);
+
   };
 
   function resetStyleName () {
-    document.querySelector('.inputName').value = "";
+
+    if (document.querySelector('.inputName').value == 'Введите ваше имя') {
+      document.querySelector('.inputName').value = "";
+    };
+
     document.querySelector('.inputName').style.color = "black";
   };
   function resetStylePhone () {
-   document.querySelector('.inputPhoneNumber').value = "";
-   document.querySelector('.inputPhoneNumber').style.color = "black";
+    if (document.querySelector('.inputPhoneNumber').value == '+7 (___) ___-__-__') {
+      document.querySelector('.inputPhoneNumber').value = "";
+      document.querySelector('.inputPhoneNumber').style.color = "black";
+    };
   };
+
+
 
 }());
