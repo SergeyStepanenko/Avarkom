2387d2a3c410cfef368d32af1adea1dc6b52c28b
SergeyStepanenko <groverfmx@gmail.com>
2017-03-03 14:31:36 +0300

    123

--- gulpfile.js
+++ gulpfile.js
@@ -6,41 +6,47 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify');
     browserSync = require('browser-sync'),
     cssnano = require('gulp-cssnano'),
-    importCss = require('gulp-import-css');
+    importCss = require('gulp-import-css'),
+    gulpCopy = require('gulp-copy');
+
+// gulp.task('copy', function () {
+//   gulp.src('app/index.html')
+//     .pipe(gulp.dest(''))
+// });
 
 gulp.task('css-import-nano', function () {
-  gulp.src('app/css/avarkom.css')
+  gulp.src('css/avarkom.css')
     .pipe(importCss())
     .pipe(cssnano())
-    .pipe(gulp.dest('app'))
+    .pipe(gulp.dest(''))
     .pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('sass', function() {
-  return gulp.src('app/sass/*.sass')
+  return gulp.src('sass/*.sass')
   .pipe(sass())
-  .pipe(gulp.dest('app'))
+  .pipe(gulp.dest(''))
 });
 
 gulp.task('scripts', function() {
-  return gulp.src('app/js/*.js')
+  return gulp.src('js/*.js')
     .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
     .pipe(uglify()) // Сжимаем JS файл
-    .pipe(gulp.dest('app')) // Выгружаем в папку app/js
+    .pipe(gulp.dest('')) // Выгружаем в папку app/js
     .pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('browser-sync', function() {
   browserSync({
     server: {
-      baseDir: 'app'
+      baseDir: ''
     },
     notify: true
   });
 });
 
 gulp.task('watch', ['browser-sync', 'css-import-nano', 'scripts'], function() {
-  gulp.watch('app/css/*.css', ['css-import-nano'])
-  gulp.watch('app/*.html', browserSync.reload)
-  gulp.watch('app/js/*.js', ['scripts'])
+  gulp.watch('css/*.css', ['css-import-nano'])
+  gulp.watch('*.html', browserSync.reload)
+  gulp.watch('js/*.js', ['scripts'])
 });
