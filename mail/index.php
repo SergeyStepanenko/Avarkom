6f9fdc41f153fdda08c61197454a7fb82cf754ef
SergeyStepanenko <groverfmx@gmail.com>
2017-03-09 12:49:39 +0300

    1

--- mail/index.php
+++ mail/index.php
@@ -1,21 +1,23 @@
 <?
 if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){
-  $to = 'grover2006@yandex.ru'; //Почта получателя, через запятую можно указать сколько угодно адресов
-  $subject = 'Обратный звонок'; //Загаловок сообщения
-  $message = '
-  <html>
-  <head>
-      <title>'.$subject.'</title>
-  </head>
-  <body>
-      <p>Email: '.$_POST['name'].'</p>
-      <p>Телефон: '.$_POST['phone'].'</p>
-  </body>
-  </html>'; //Текст нащего сообщения можно использовать HTML теги
-  $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
-  $headers .= "From: Отправитель <info@dtp-help.ru>\r\n"; //Наименование и почта отправителя
-  mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+$to = 'grover2006@yandex.ru, groverfmx@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+$subject = 'Обратный звонок'; //Загаловок сообщения
+$email = strip_tags($_POST['name']);
+$phone = strip_tags($_POST['phone']);
+$message = '
+<html>
+<head>
+<title>'.$subject.'</title>
+</head>
+<body>
+<p>Email: '.$name.'</p>
+<p>Телефон: '.$phone.'</p>
+</body>
+</html>'; //Текст нащего сообщения можно использовать HTML теги
+$headers = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+$headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
+mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
 
-  header('Location: http://dtp-help-expert.ru/?send=true');
+header('Location: http://avakomm.ru/?send=true');
 }
 ?>
